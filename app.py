5ba8e22dd26d054d0d58858bbe12c37419a56d4b
Pradit Yamsook <pradit.y@ku.th>
2019-04-22 01:38:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,6 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
     '/uploads':  app.config['UPLOAD_FOLDER']
 })
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     app.debug=False
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000)"""
