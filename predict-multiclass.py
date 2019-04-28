cbd5819a013fcc4cc643629ce8dd1f4710f085bc
pradityamsook <pradit.y@ku.th>
2019-04-29 00:06:22 +0700

    add Rapsberry Class

--- predict-multiclass.py
+++ predict-multiclass.py
@@ -19,12 +19,10 @@ def predict(file):
   array = model.predict(x)
   result = array[0]
   answer = np.argmax(result)
-  if answer == 0:
-    print("Label: Cherry Rainer")
-  elif answer == 1:
-    print("Label: Cherry Wax Black")
-  elif answer == 2:
-    print("Label: Cherry Wax Red")
+  if answer == 0 or answer == 1 or answer == 2:
+    print("Label: Cherry")
+	elif answer == 3:
+		print("Label: Raspberry")
 
   return answer
 
@@ -69,12 +67,27 @@ for i, ret in enumerate(os.walk('./test-data/CherryWaxRed')):
     else:
       Cherry_Wax_Red_f += 1
 
+for i, ret in enumerate(os.walk('./test-data/Raspberry')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    #print("Label: Cherry Wax Red")
+    result = predict(ret[0] + '/' + filename)
+    if result == 2:
+      print(ret[0] + '/' + filename)
+      Raspberry_t += 1
+    else:
+      Raspberry_f += 1
+
 """
 Check metrics
 """
-print("True Cherry Rainier: ", Cherry_Rainier_t)
-print("False Cherry Rainier: ", Cherry_Rainier_f)
-print("True Cherry Wax Black: ", Cherry_Wax_Black_t)
-print("False Cherry Wax Black: ", Cherry_Wax_Black_f)
-print("True Cherry Wax Red: ", Cherry_Wax_Red_t)
-print("False Cherry Wax Red: ", Cherry_Wax_Red_f)
+print("True Cherry: ", Cherry_Rainier_t)
+print("False Cherry: ", Cherry_Rainier_f)
+print("True Cherry: ", Cherry_Wax_Black_t)
+print("False Cherry: ", Cherry_Wax_Black_f)
+print("True Cherry: ", Cherry_Wax_Red_t)
+print("False Cherry: ", Cherry_Wax_Red_f)
+
+print("True Raspberry: ", Raspberry_t)
+print("False Raspberry: ", Raspberry_f)
